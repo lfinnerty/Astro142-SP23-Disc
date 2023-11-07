2d7406b937cd71808ad4af9cda0ec2043cba5ca9
lfinnerty <finnerty.luke@gmail.com>
2023-11-06 21:12:24 -0800

    updating demo

--- Discussion11/discussion11_demo.py
+++ Discussion11/discussion11_demo.py
@@ -4,5 +4,9 @@ from astropy.io import ascii
 import astropy.units as u
 
 data = ascii.read('autoread_table.txt')
-data['a (AU)'].unit = u.AU
 print(data)
+### Note that lines are 0 indexed and empty lines removed!
+data2 = ascii.read('manualread_table.txt',header_start=4,data_start=5,data_end=13)
+print(data2)
+
+data2.write('output.txt',format='latex')
